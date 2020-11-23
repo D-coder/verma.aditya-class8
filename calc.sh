e8405eb1bf59c7f55ac43d46af814a651005250a
Adeeth Verma <addyv@Adeeths-MacBook-Pro.local>
2020-11-23 11:59:25 -0500

    Now proceeding with subtraction

--- calc.sh
+++ calc.sh
@@ -20,3 +20,12 @@ sum=$(( $num1 + $num2 ))
  
 echo "Sum is: $sum"  
 
+
+#subtract
+echo "Subtraction: "
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+ 
+sub =$(( $num1 - $num2 ))
+ 
+echo "Subtraction is: $sub"  
