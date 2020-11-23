a68d194a13e3efe9d43893b37557db82a4721c42
Adeeth Verma <addyv@Adeeths-MacBook-Pro.local>
2020-11-23 11:57:20 -0500

    Just tried to initialise the folder manually

---
+++ calc.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# When the program is first loaded, display a greeting to the user.
+# Then, display a menu that outlines the possible operations:
+  # Add
+
+  # Subtract
+  # Exit
+# Then, capture the user selection.
+# If the selection matches a supported operation, execute the operation.
+# If the selection does not match a support operation, display an error message.
+# When the operation is complete, redisplay the menu.
+#
+#ADD
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+ 
+sum=$(( $num1 + $num2 ))
+ 
+echo "Sum is: $sum"  
+
